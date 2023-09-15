bee365dea7f32342cb26853324e5c69057eee1a0
ayleenrq <qisthyayleen@gmail.com>
2023-09-15 14:07:51 +0700

    P3_Percobaan1_Pertanyaan1

--- src/P3/ContohVariabel07.java
+++ src/P3/ContohVariabel07.java
@@ -1,12 +1,12 @@
 package P3;
 public class ContohVariabel07 {
     public static void main(String[] args) throws Exception {
-        String salahSatuHobySayaAdalah = "Bermain petak umpet";
+        String Hobby = "Bermain petak umpet";
         boolean isPandai = true;
         char jenisKelamin = 'L';
         byte _umurSayaSekarang = 20;
         double $ipk = 3.24, tinggi = 1.78;
-        System.out.println(salahSatuHobySayaAdalah);
+        System.out.println("Hobby saya: " + Hobby);
         System.out.println("Apakah pandai? " + isPandai);
         System.out.println("Jenis kelamin: " + jenisKelamin);
         System.out.println("Umurku saat ini: " + _umurSayaSekarang);
