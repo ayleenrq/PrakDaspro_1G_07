dd855c9140772eb7dda79ae34d3daab2645bf1f8
ayleenrq <qisthyayleen@gmail.com>
2023-09-22 19:32:41 +0700

    P4_Percobaan3_Pertanyaan2

--- src/P4/HargaBayar07.java
+++ src/P4/HargaBayar07.java
@@ -3,32 +3,33 @@ import java.util.Scanner;
 
 public class HargaBayar07 {
     public static void main(String[] args) {
-        Scanner sc07 =new Scanner(System.in);
+        Scanner input =new Scanner(System.in);
 
         String merkBuku;
-        int harga, jumlah, jmlHalBuku, dis;
-        double total, bayar, jmlDis;
+        int harga, jumlah, jmlHalBuku;
+        double total, bayar, dis, jmlDis;
 
         System.out.println("Masukkan Merk Buku: ");
-        merkBuku=sc07.nextLine();
+        merkBuku=input.nextLine();
 
         System.out.println("Masukkan Jumlah Halaman Buku: ");
-        jmlHalBuku=sc07.nextInt();
+        jmlHalBuku=input.nextInt();
 
         System.out.println("Masukkan harga barang yang dibeli: ");
-        harga=sc07.nextInt();
+        harga=input.nextInt();
 
         System.out.println("Masukkan jumlah barang yang dibeli: ");
-        jumlah=sc07.nextInt();
+        jumlah=input.nextInt();
 
         System.out.println("Masukkan jumlah diskon: ");
-        dis=sc07.nextInt();
-        
+        dis=input.nextInt();
+
         total = harga * jumlah;
         jmlDis = total * (dis/100);
         bayar = total - jmlDis;
-
+        
         System.out.println("Diskon yang anda dapatkan adalah: " + jmlDis);
         System.out.println("Jumlah yang harus dibayar adalah: " + bayar);
+        input.close();
     }
 }
