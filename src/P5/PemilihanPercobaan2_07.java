5885ed92983ff5405f90dac375d9f54263d748e3
ayleenrq <qisthyayleen@gmail.com>
2023-10-06 16:04:44 +0700

    P5_Percobaan2_Pertanyaan1

--- src/P5/PemilihanPercobaan2_07.java
+++ src/P5/PemilihanPercobaan2_07.java
@@ -6,7 +6,8 @@ public class PemilihanPercobaan2_07 {
         Scanner input07 = new Scanner(System.in);
 
         float uas, uts, kuis, tugas, total;
-        String nilaiAkhir;
+        String hasil;
+        int nilai;
 
         System.out.print("Masukkan Nilai UAS   : ");
         uas = input07.nextFloat();
@@ -22,7 +23,30 @@ public class PemilihanPercobaan2_07 {
 
         total = (uas * 0.4F) + (uts * 0.3F) + (kuis * 0.1F) + (tugas * 0.2F);
 
-        nilaiAkhir = (total < 65) ? "remidi" : "tidak remidi";
-        System.out.println("Nilai Akhir : " +total+ " sehingga anda " +nilaiAkhir);
+        hasil = (total < 65) ? "remidi" : "tidak remidi";
+        System.out.println("Nilai Akhir : " +total+ " sehingga anda " +hasil);
+        
+        nilai=(int)total;
+        if (nilai > 80 && nilai <= 100){
+            System.out.println("Nilai anda A setara IPK 4 dengan Kualifikasi Sangat Baik");
+        }
+        else if (nilai > 73 && nilai <= 80){
+            System.out.println("Nilai anda B+ setara IPK 3.5 dengan Kualifikasi Lebih dari Baik");
+        }
+        else if (nilai > 65 && nilai <= 73){
+            System.out.println("Nilai anda B setara IPK 3 dengan Kualifikasi Baik");
+        }
+        else if (nilai > 60 && nilai <= 65){
+            System.out.println("Nilai anda C+ setara IPK 2,5 dengan Kualifikasi Lebih dari Cukup");
+        }
+        else if (nilai > 50 && nilai <= 60){
+            System.out.println("Nilai anda C setara IPK 2 dengan Kualifikasi Cukup");
+        }
+        else if (nilai > 39 && nilai <= 50){
+            System.out.println("Nilai anda D setara IPK 1 dengan Kualifikasi Kurang");
+        }
+        else {
+            System.out.println("Nilai anda E setara IPK 0 dengan Kualifikasi Gagal");
+        }
     }
 }
