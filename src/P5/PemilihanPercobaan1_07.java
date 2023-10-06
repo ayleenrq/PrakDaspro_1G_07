093d537f17d7ac66a43829c8b8b8c1c5b0891447
ayleenrq <qisthyayleen@gmail.com>
2023-10-06 14:07:45 +0700

    P5_Percobaan1_Pertanyaan1

--- src/P5/PemilihanPercobaan1_07.java
+++ src/P5/PemilihanPercobaan1_07.java
@@ -5,15 +5,10 @@ public class PemilihanPercobaan1_07 {
     public static void main(String[] args) {
         Scanner input07 = new Scanner(System.in);
 
-        int angka;
-
         System.out.print("Masukkan Angka: ");
-        angka = input07.nextInt();
+        int angka = input07.nextInt();
 
-        if (angka % 2 == 0)
-            System.out.println("Angka " +angka+ " bilangan genap");
-            
-        else
-            System.out.println("Angka " +angka+ " bilangan ganjil");
+        String jenisAngka = (angka % 2 == 0) ? "genap" : "ganjil" ;
+        System.out.println("Angka " +angka+ " bilangan " +jenisAngka);
     }
 }
