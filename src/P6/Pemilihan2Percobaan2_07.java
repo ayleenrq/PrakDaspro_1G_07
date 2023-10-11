84c5e139666391412648f15ea00c2506be7d8adb
ayleenrq <qisthyayleen@gmail.com>
2023-10-11 15:24:06 +0700

    P6_Percobaan2_Pertanyaan2

--- src/P6/Pemilihan2Percobaan2_07.java
+++ src/P6/Pemilihan2Percobaan2_07.java
@@ -17,12 +17,21 @@ public class Pemilihan2Percobaan2_07 {
         float totalSudut = sudut1 + sudut2 + sudut3;
 
         if (totalSudut == 180) {
-            if ((sudut1 == 90) || (sudut2 == 90) || (sudut3 == 90))
-                System.out.println("Segitiga Siku-Siku");
+            if ((sudut1 == 90) || (sudut2 == 90) || (sudut3 == 90)) {
+                if ((sudut1 == sudut2) || (sudut1 == sudut3) || (sudut2 == sudut3)) {
+                    System.out.println("Segitiga Siku-Siku dan Sama Kaki");
+                } else
+                    System.out.println("Segitiga Siku-Siku");
+            }
+            else if ((sudut1 == sudut2) && (sudut2 == sudut3)) {
+                System.out.println("Segitiga Sama Sisi");
+            }
+            else if ((sudut1 == sudut2) || (sudut1 == sudut3) || (sudut2 == sudut3)) {
+                System.out.println("Segitiga Sama Kaki");
+            }
             else
-                System.out.println("Bukan Segitiga Siku-Siku");
-        }
-        else
+                System.out.println("Segitiga Sembarang");
+        } else
             System.out.println("Bukan Segitiga");
     }
 }
