3fd7a0110ff163783e6ca914ec84996a0f267dac
ayleenrq <qisthyayleen@gmail.com>
2023-10-11 12:48:02 +0700

    P6_Percobaan1_07

---
+++ src/P6/Pemilihan2Percobaan1_07.java
@@ -0,0 +1,19 @@
+package P6;
+import java.util.Scanner;
+
+public class Pemilihan2Percobaan1_07 {
+    public static void main(String[] args) {
+        Scanner input07 = new Scanner(System.in);
+
+        System.out.print("Masukkan Tahun: ");
+        int tahun = input07.nextInt();
+
+        if ((tahun % 4) == 0){
+            if ((tahun % 100) != 0){
+                System.out.println("Tahun Kabisat");
+            }
+        } else
+            System.out.println("Bukan Tahun Kabisat");
+        input07.close();
+    }
+}
