7a2b4752a0172985c2b1f263ee449f4318b54bf6
ayleenrq <qisthyayleen@gmail.com>
2023-10-13 15:58:04 +0700

    P7_Percobaan2_07

---
+++ src/P7/WhileGaji07.java
@@ -0,0 +1,42 @@
+package P7;
+import java.util. Scanner;
+
+public class WhileGaji07 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int jumlahKaryawan, jumlahJamLembur;
+        double gajiLembur = 0, totalGajiLembur = 0;
+        String jabatan;
+        jabatan= "Direktur";
+        jabatan= "Manager"; 
+        jabatan= "karyawan";
+
+        System.out.print("Masukkan jumlah karyawan: ");
+        jumlahKaryawan = scan.nextInt();
+
+        int i = 0;
+        while (i < jumlahKaryawan) {
+            System.out.println("Pilihan jabatan - Direktur, Manager, Karyawan");
+            System.out.print("Masukkan jabatan karyawan ke-" + (i+1) + ": ");
+            jabatan = scan.next();
+
+            System.out.print("Masukkan jumlah jam lembur: ");
+            jumlahJamLembur = scan.nextInt();
+            i++;
+
+            if (jabatan.equalsIgnoreCase("Direktur")) {
+                continue;
+            }
+            else if (jabatan.equalsIgnoreCase("Manager")) {
+                gajiLembur = jumlahJamLembur * 100000;
+            }
+            else if (jabatan.equalsIgnoreCase("Karyawan")) {
+                gajiLembur = jumlahJamLembur * 75000;
+            }
+            totalGajiLembur += gajiLembur;
+            System.out.println("Total gaji lembur: " + totalGajiLembur);
+        }
+        scan.close();
+    }
+}
