ca8e07c6c692c0f318f1f52b658fb8ac0fb8f11a
ayleenrq <qisthyayleen@gmail.com>
2023-10-13 16:32:37 +0700

    P7_Percobaan3_07

---
+++ src/P7/DoWhileCuti07.java
@@ -0,0 +1,33 @@
+package P7;
+import java.util.Scanner;
+
+public class DoWhileCuti07 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String konfirmasi;
+        int jatahCuti, jumlahHari;
+
+        System.out.print("Jatah cuti: ");
+        jatahCuti = sc.nextInt();
+
+        do {
+            System.out.print("Apakah anda ingin mengambil cuti (y/t)? ");
+            konfirmasi = sc.next();
+
+            if (konfirmasi.equalsIgnoreCase("y")); {
+                System.out.print("Jumlah hari: ");
+                jumlahHari = sc.nextInt();
+
+                if (jumlahHari <= jatahCuti) {
+                    jatahCuti -= jumlahHari;
+                    System.out.println("Sisa jatah cuti: " + jatahCuti);
+                }
+                else {
+                    System.out.println("Sisa jatah cuti anda tidak mencukupi");
+                    break;
+                }
+            }
+        } while (jatahCuti > 0);
+    }    
+}
