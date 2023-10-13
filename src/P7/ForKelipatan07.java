8e81f6cd236f77ad2943a733b19fb060032273f9
ayleenrq <qisthyayleen@gmail.com>
2023-10-13 15:49:36 +0700

    P7_Percobaan1_Pertanyaan3

--- src/P7/ForKelipatan07.java
+++ src/P7/ForKelipatan07.java
@@ -6,6 +6,7 @@ public class ForKelipatan07 {
         Scanner scan = new Scanner(System.in);
 
         int kelipatan, jumlah = 0, counter = 0;
+        float rataRata;
 
         System.out.print("Masukkan bilangan kelipatan (1-9): ");
         kelipatan = scan.nextInt();
@@ -16,7 +17,10 @@ public class ForKelipatan07 {
                 counter++;
             }
         }
+        rataRata = (float ) jumlah / counter;
+
         System.out.printf("Banyaknya bilangan %d dari 1 dari 50 adalah %d\n", kelipatan, counter);
         System.out.printf("Total bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, jumlah);
+        System.out.printf("Rata-rata dari bilangan kelipatan %d dari 1 sampai 50 adalah %.2f\n", kelipatan, rataRata);
     }
 }
