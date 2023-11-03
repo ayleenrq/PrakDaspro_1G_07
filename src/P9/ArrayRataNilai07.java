8444ba86df9ebb38a60e054423b6e17dc2174c90
ayleenrq <qisthyayleen@gmail.com>
2023-11-03 20:17:42 +0700

    P9_Percobaan3_Pertanyaan2

--- src/P9/ArrayRataNilai07.java
+++ src/P9/ArrayRataNilai07.java
@@ -1,36 +1,36 @@
 package P9;
 import java.util.Scanner;
-
 public class ArrayRataNilai07 {
     public static void main(String[] args) {
         Scanner ayleen07 = new Scanner(System.in);
 
-        int[] nilaiMhs = new int[10];
-        int banyakLulus = 0;
-        double total = 0;
-        double rata2;
+        int[] nilaiMhs;
+        int jumlahMahasiswa, banyakLulus = 0, banyakTidakLulus = 0;
+        double totalLulus = 0, totalTdkLulus = 0;
+        double rataLulus=0, rataTdkLulus=0;
+
+        System.out.print("Masukkan jumlah mahasiswa : ");
+        jumlahMahasiswa = ayleen07.nextInt();
+        nilaiMhs = new int[jumlahMahasiswa];
 
         for (int i = 0; i < nilaiMhs.length; i++) {
-            System.out.print("Masukkan n ilai mahasiswa ke-" + (i+1) + " : ");
+            System.out.print("Masukkan nilai mahasiswa ke-" + (i+1) + " : ");
             nilaiMhs[i] = ayleen07.nextInt();
         }
 
         for (int i = 0; i < nilaiMhs.length; i++) {
-            total += nilaiMhs[i];
             if (nilaiMhs[i] > 70) {
-                System.out.println("Mahasiswa ke-" + i + " lulus!");
-            }
-        }
-
-        for  (int i = 0; i < nilaiMhs.length; i++) {
-            if (nilaiMhs[i] > 70) {
+                totalLulus += nilaiMhs[i];
                 banyakLulus++;
+            } else {
+                totalTdkLulus += nilaiMhs[i];
+                banyakTidakLulus++;
             }
         }
-
-        System.out.println("Banyaknya mahasiswa yang lulus adalah " + banyakLulus);
-
-        rata2 = total / nilaiMhs.length;
-        System.out.println("Rata - rata nilai = " + rata2);
+        rataLulus = totalLulus / banyakLulus;
+        rataTdkLulus = totalTdkLulus / banyakTidakLulus;
+    
+        System.out.println("Rata-rata nilai lulus = " + rataLulus);
+        System.out.println("Rata-rata nilai tidak lulus = " + rataTdkLulus);
     }
 }
