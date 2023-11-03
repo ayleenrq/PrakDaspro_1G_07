d43e3335b96a976902d5e125b752c7d4bd9718f1
ayleenrq <qisthyayleen@gmail.com>
2023-11-03 20:45:18 +0700

    P9_Percobaan4_Pertanyaan2_07

--- src/P9/LinearSearch07.java
+++ src/P9/LinearSearch07.java
@@ -1,17 +1,32 @@
 package P9;
+import java.util.Scanner;
 
 public class LinearSearch07 {
     public static void main(String[] args) {
-        int[] arrayInt = {34, 18, 26, 48, 72, 20, 56, 63};
-        int key = 20;
+        Scanner ayleen07 = new Scanner(System.in);
+
+        int[] arrayInt;
+        int key = 0;
         int hasil = 0;
 
+        System.out.print("Masukkan jumlah elemen array : ");
+        int jumlah = ayleen07.nextInt();
+        arrayInt = new int[jumlah];
+
         for (int i = 0; i < arrayInt.length; i++) {
+            System.out.print("Masukkan elemen array ke-" + (i+1) + " : ");
+            arrayInt[i] = ayleen07.nextInt();
+        }
+
+        System.out.print("Masukkan key yang ingin dicari : ");
+        key = ayleen07.nextInt();
+        
+            for (int i = 0; i < arrayInt.length; i++) {
             if (arrayInt[i] == key) {
                 hasil = i;
                 break;
             }
         }
-        System.out.println("Key ada dalam array pada indeks ke-" + hasil);
+        System.out.println("Key ada di posisi indeks ke-" + hasil);
     }
 }
