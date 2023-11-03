58a7b2609bc885ac501190263275393142aa44c8
ayleenrq <qisthyayleen@gmail.com>
2023-11-03 19:25:04 +0700

    P9_Percobaan3_Pertanyaan1

--- src/P9/ArrayRataNilai07.java
+++ src/P9/ArrayRataNilai07.java
@@ -6,17 +6,29 @@ public class ArrayRataNilai07 {
         Scanner ayleen07 = new Scanner(System.in);
 
         int[] nilaiMhs = new int[10];
+        int banyakLulus = 0;
         double total = 0;
         double rata2;
 
         for (int i = 0; i < nilaiMhs.length; i++) {
-            System.out.print("Masukkan nilai mahasiswa ke-" + (i+1) + " : ");
+            System.out.print("Masukkan n ilai mahasiswa ke-" + (i+1) + " : ");
             nilaiMhs[i] = ayleen07.nextInt();
         }
 
         for (int i = 0; i < nilaiMhs.length; i++) {
             total += nilaiMhs[i];
+            if (nilaiMhs[i] > 70) {
+                System.out.println("Mahasiswa ke-" + i + " lulus!");
+            }
         }
+
+        for  (int i = 0; i < nilaiMhs.length; i++) {
+            if (nilaiMhs[i] > 70) {
+                banyakLulus++;
+            }
+        }
+
+        System.out.println("Banyaknya mahasiswa yang lulus adalah " + banyakLulus);
 
         rata2 = total / nilaiMhs.length;
         System.out.println("Rata - rata nilai = " + rata2);
