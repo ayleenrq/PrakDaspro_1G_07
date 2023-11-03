5905e6c350562170b1b290c0b2a799b0b4b9888b
ayleenrq <qisthyayleen@gmail.com>
2023-11-03 15:41:34 +0700

    P9_Percobaan2_Pertanyaan3

--- src/P9/ArrayNilai07.java
+++ src/P9/ArrayNilai07.java
@@ -12,8 +12,10 @@ public class ArrayNilai07 {
             nilaiAkhir[i] = ayleen07.nextInt();
         }
 
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Nilai akhir ke-" + i + " adalah " + nilaiAkhir[i]);
+        for (int i = 0; i < nilaiAkhir.length; i++) {
+            if (nilaiAkhir[i] > 70) {
+                System.out.println("Mahasiswa ke-" + i + " lulus!");
+            }
         }
     }
 }
