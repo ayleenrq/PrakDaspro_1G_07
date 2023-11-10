026109b41e21e2dcb9236a4b0893f5af0f95c928
ayleenrq <qisthyayleen@gmail.com>
2023-11-10 14:28:43 +0700

    P10_Percobaan1_Pertanyaan6_07

--- src/P10/Bioskop07.java
+++ src/P10/Bioskop07.java
@@ -13,8 +13,9 @@ public class Bioskop07 {
         penonton[3][1] = "Hana";
 
         System.out.println(penonton.length);
-        for (int i = 0; i < penonton.length; i++) {
-            System.out.println("Panjang baris ke-" + (i+1) + " : " + penonton[i].length);
+
+        for (String[] barisPenonton : penonton) {
+            System.out.println("Panjang baris : " + barisPenonton.length);
         }
     }
 }
