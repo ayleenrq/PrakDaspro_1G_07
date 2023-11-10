b8413fd643698208fcf91e24be6bc8a63c90fe02
ayleenrq <qisthyayleen@gmail.com>
2023-11-10 16:07:19 +0700

    P10_Percobaan2_Pertanyaan2_07

--- src/P10/BioskopWithScanner07.java
+++ src/P10/BioskopWithScanner07.java
@@ -5,29 +5,51 @@ public class BioskopWithScanner07 {
     public static void main(String[] args) {
         Scanner ayleen07 = new Scanner(System.in);
 
-        String nama, next;
+        String nama, next, menu;
         int baris, kolom;
-        String[][] penonton = new String[4][2];
+        String[][] penonton = new String[5][5];
+        boolean exit = false;
 
-        while (true) {
-            System.out.print("Masukkan nama : ");
-            nama = ayleen07.nextLine();
+        do {
+            System.out.println("Menu 1: Input data penonton");
+            System.out.println("Menu 2: Tampilkan daftar penonton");
+            System.out.println("Menu 3: Exit");
+            System.out.print("Masukkan Menu (1/2/3) : ");
+            menu = ayleen07.nextLine();
 
-            System.out.print("Masukkan baris : ");
-            baris = ayleen07.nextInt();
-
-            System.out.print("Masukkan kolom : ");
-            kolom = ayleen07.nextInt();
-            ayleen07.nextLine();
-
-            penonton[baris -1][kolom-1] = nama;
-
-            System.out.print("Input penonton lainnya? (y/n) : ");
-            next = ayleen07.nextLine();
-
-            if (next.equalsIgnoreCase("n")) {
+            switch (menu) {
+                case "1":
+                while (true) {
+                    System.out.print("Masukkan nama : ");
+                    nama = ayleen07.nextLine();
+                    System.out.print("Masukkan baris : ");
+                    baris = ayleen07.nextInt();
+                    System.out.print("Masukkan kolom : ");
+                    kolom = ayleen07.nextInt();
+                    ayleen07.nextLine();
+                   
+                    penonton[baris -1][kolom-1] = nama;
+    
+                    System.out.print("Input penonton lainnya? (y/n) : ");
+                    next = ayleen07.nextLine();
+        
+                    if (next.equalsIgnoreCase("n")) {
+                        break;
+                    }
+                }
+                break;
+                case "2":
+                    for (int i = 0; i < penonton.length; i++) {
+                        System.out.println("Penonton pada baris ke-" + (i+1) + " : " + String.join(", ", penonton[i]));
+                    }
+                break;
+                case "3":
+                    exit = true;
+                break;
+                default:
+                    System.out.println("Pilihan tidak valid!");
                 break;
             }
-        }
+        } while (exit != true);
     }    
 }
