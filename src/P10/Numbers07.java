456745214050f9c119a06b32a67ac2499ff68769
ayleenrq <qisthyayleen@gmail.com>
2023-11-10 20:17:22 +0700

    P10_Percobaan3_07

---
+++ src/P10/Numbers07.java
@@ -0,0 +1,10 @@
+package P10;
+
+public class Numbers07 {
+    public static void main(String[] args) {
+        int[][] numbers = new int[3][];
+        numbers[0] = new int[5];
+        numbers[1] = new int[3];
+        numbers[2] = new int[1];
+    }
+}
