26a7764deca9dd1c6dff258eea0ed2d575743e90
ayleenrq <qisthyayleen@gmail.com>
2023-11-15 13:56:39 +0700

    P11_Percobaan1_Pertanyaan2_07

--- src/P11/Star07.java
+++ src/P11/Star07.java
@@ -8,7 +8,7 @@ public class Star07 {
         System.out.print("Masukkan nilai N : ");
         int N = ayleen07.nextInt();
 
-        for (int i = 0; i <= N; i++) {
+        for (int i = 0; i > N; i++) {
             System.out.print("*");
         }
     }
