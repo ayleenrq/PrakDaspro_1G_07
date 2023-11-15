dadf6607acc0e2d045f3fbf9f42941a15f660bdd
ayleenrq <qisthyayleen@gmail.com>
2023-11-15 14:50:59 +0700

    P11_Percobaan2_07

---
+++ src/P11/Square07.java
@@ -0,0 +1,18 @@
+package P11;
+import java.util.Scanner;
+
+public class Square07 {
+    public static void main(String[] args) {
+        Scanner ayleen07 = new Scanner(System.in);
+
+        System.out.print("Masukkan nilai N : ");
+        int nilai = ayleen07.nextInt();
+
+        for (int i = 1; i <= nilai; i++) {
+            for (int j = 1; j <= nilai; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
