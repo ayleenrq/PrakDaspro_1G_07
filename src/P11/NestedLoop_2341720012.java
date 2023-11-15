6885341e8f61362a5fde06e319f88d3304d5c3ae
ayleenrq <qisthyayleen@gmail.com>
2023-11-15 15:45:32 +0700

    P11_Percobaan3_07

---
+++ src/P11/NestedLoop_2341720012.java
@@ -0,0 +1,25 @@
+package P11;
+import java.util.Scanner;
+
+public class NestedLoop_2341720012 {
+    public static void main(String[] args) {
+        Scanner ayleen07 = new Scanner(System.in);
+        int[][] array = new int[5][7];
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("Kota ke-" + (i+1));
+            for (int j = 0; j < array[0].length; j++) {
+                System.out.print("Hari ke-" + (j+1) + " : ");
+                array[i][j] = ayleen07.nextInt();
+            }
+            System.out.println();
+        }
+        for (int i = 0; i < array.length; i++) {
+            System.out.print("Kota ke-" + (i+1) + " : ");
+            for (int j = 0; j < array.length; j++) {
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
