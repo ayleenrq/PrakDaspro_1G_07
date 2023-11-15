0e451eaa22839dfe874d81492542f2554131bf31
ayleenrq <qisthyayleen@gmail.com>
2023-11-15 22:59:03 +0700

    P11_Percobaan3_Pertanyaan3_07

--- src/P11/NestedLoop_2341720012.java
+++ src/P11/NestedLoop_2341720012.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class NestedLoop_2341720012 {
     public static void main(String[] args) {
         Scanner ayleen07 = new Scanner(System.in);
-        int[][] array = new int[5][7];
+        int[][] array = new int[2][3];
 
         for (int i = 0; i < array.length; i++) {
             System.out.println("Kota ke-" + (i+1));
@@ -16,10 +16,14 @@ public class NestedLoop_2341720012 {
         }
         for (int i = 0; i < array.length; i++) {
             System.out.print("Kota ke-" + (i+1) + " : ");
+            int total = 0;
             for (int j = 0; j < array[0].length; j++) {
+                total += array[i][j];
                 System.out.print(array[i][j] + " ");
             }
             System.out.println();
+            double rataRata = (double) total / array[0].length;
+            System.out.println("Rata-rata Kota ke-" + (i+1) + " : " + rataRata);
         }
     }
 }
