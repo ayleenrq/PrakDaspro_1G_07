957449cff9bfdffa023f5b21f7b83790fda33154
ayleenrq <qisthyayleen@gmail.com>
2023-11-17 15:56:53 +0700

    P11_Tugas4_07

---
+++ src/P11/Tugas4_07.java
@@ -0,0 +1,22 @@
+package P11;
+
+public class Tugas4_07 {
+    public static void main(String[] args) {
+        char[] nama = {'A', 'y', 'l', 'e', 'e', 'n', 'R', 'u', 'h', 'u', 'l', 'Q', 'i', 's', 't', 'h', 'y'};
+        char[][] namaArray = new char[8][5];
+        int index = 0;
+
+        for (int i = 0; i < namaArray.length; i++) {
+            for (int j = 0; j < namaArray[0].length; j++) {
+                if (index < nama.length) {
+                    namaArray[i][j] = nama[index];
+                    index++;
+                    System.out.print(namaArray[i][j] + "  ");
+                } else {
+                    index = 0;
+                }
+            }
+            System.out.println();
+        }
+    }
+}
