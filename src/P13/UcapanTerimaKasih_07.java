e110462fa56faee41a53fd4dea61c7e98d019f2f
ayleenrq <qisthyayleen@gmail.com>
2023-12-01 14:51:29 +0700

    P13_Percobaan4_Pertanyaan3_07

--- src/P13/UcapanTerimaKasih_07.java
+++ src/P13/UcapanTerimaKasih_07.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 public class UcapanTerimaKasih_07 {
     public static void main(String[] args) {
         UcapanTerimaKasih();
+        String ucapan = "I hope your wish comes true";
+        UcapanTambahan(ucapan);
     }
     public static String PenerimaUcapan() {
         Scanner ayleen07 = new Scanner(System.in);
@@ -17,5 +19,9 @@ public class UcapanTerimaKasih_07 {
         String nama = PenerimaUcapan();
         System.out.println("Thank you "+ nama +" for being the best teacher in the wolrd.\n" +
         "You inspired in me a love for learning and made me feel like I could ask you anything.");
+        String ucapan = "I hope your wish comes true";
+    }
+    public static void UcapanTambahan(String ucapan) {
+        System.out.println(ucapan);
     }
 }
