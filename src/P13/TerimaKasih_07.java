09e7babc2191e97d9d6b3ed4a148242126a92d53
ayleenrq <qisthyayleen@gmail.com>
2023-12-01 13:40:39 +0700

    P13_Percobaan1_Pertanyaan2_07

--- src/P13/TerimaKasih_07.java
+++ src/P13/TerimaKasih_07.java
@@ -2,9 +2,6 @@ package P13;
 
 public class TerimaKasih_07 {
     public static void main(String[] args) {
-        UcapanTerimaKasih();
-    }
-    public static void UcapanTerimaKasih() {
         System.out.println("Thank you for being the best teacher in the wolrd.\n" +
         "You inspired in me a love for learning and made me feel like I could ask you anything.");
     }
